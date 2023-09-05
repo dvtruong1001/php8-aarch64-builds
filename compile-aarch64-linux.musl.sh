c894d0a286da38458cde29a0868049ecd73a7fcf
AEP <91467886+TukangM@users.noreply.github.com>
2023-09-05 14:56:34 +0000

    modified repo

---
+++ compile-aarch64-linux.musl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install make autoconf automake libtool-bin m4 wget gzip bzip2 bison g++ git cmake pkg-config re2c ca-certificates -y
+git clone https://github.com/TukangM/musl-cross-make
+cd musl-cross-make
+sudo make install
+cd ..
