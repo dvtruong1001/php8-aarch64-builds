7177efbf562cb5a31d8df14638a4a3533e660bdd
AEP <91467886+TukangM@users.noreply.github.com>
2024-01-13 21:51:08 +0700

    Update compile-aarch64-linux.musl.sh

--- compile-aarch64-linux.musl.sh
+++ compile-aarch64-linux.musl.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt install make autoconf automake libtool-bin m4 wget gzip bzip2 bison g++ git cmake pkg-config re2c ca-certificates -y
 git clone https://github.com/TukangM/musl-cross-make
 cd musl-cross-make
-sudo make install
+sudo make install -j10
 cd ..
